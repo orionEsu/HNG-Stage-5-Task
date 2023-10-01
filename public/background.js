dc8133dfeb31a71d01c1d6100094a89c15b93a65
orionEsu <orionesu.dev@gmail.com>
2023-10-01 22:19:39 +0100

    updates

--- public/background.js
+++ public/background.js
@@ -28,6 +28,21 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 		}
 
 		// Now you have the buffer, you can send it to an endpoint
-		// sendBufferToEndpoint(buffer);
+
+		// Prepare the request
+		fetch('https://example.com/your-endpoint', {
+			method: 'POST',
+			body: buffer,
+			headers: {
+				'Content-Type': 'application/octet-stream',
+			},
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log('Response from endpoint:', data);
+			})
+			.catch((error) => {
+				console.error('Error sending buffer:', error);
+			});
 	}
 });
